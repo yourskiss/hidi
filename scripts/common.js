6b04b9a61889df7207ad94be66fa8c3de584a4a1
K. Singh <16361374+yourskiss@users.noreply.github.com>
2023-03-05 14:40:39 +0530

    ui

--- scripts/common.js
+++ scripts/common.js
@@ -10,7 +10,7 @@ $(window).on('load', function()
 {
  
 });
-/* hide video === end */
+
 
 
 $(".profileunlocks .profileunlockinner .profileunlockright ul li").click(function()
@@ -19,61 +19,43 @@ $(".profileunlocks .profileunlockinner .profileunlockright ul li").click(functio
     $(".profileunlocks .profileunlockinner .profileunlockleft div img").fadeOut(300);
     $(".profileunlocks .profileunlockinner .profileunlockright ul li").removeClass("active");
     $(this).addClass("active");
-    $(".profileunlocks .profileunlockinner .profileunlockleft div img[data-id='"+getDataID+"']").fadeIn(300);
-
-    
+    $(".profileunlocks .profileunlockinner .profileunlockleft div img[data-id='"+getDataID+"']").fadeIn(300);  
 });
 
 
-  
-// social slider   === start 
-$(function($) 
-{
-    if($(".navslider").length > 0)
-    {
-        $('.navslider').slick({
-            slidesToShow:6,
-            slidesToScroll: 1,
-            dots: false,
-            arrows: true,
-            autoplay: false,
-            autoplaySpeed: 3000,
-            infinite: false,
-            adaptiveHeight: false,
-            variableWidth: false,
-            centerMode: false,
-            centerPadding: '0',
-            initialSlide:0,
-            responsive:
+$('.talentslider').slick({
+    rows: 3,
+    speed: 6000,
+    autoplay: true,
+    autoplaySpeed: 0,
+    centerMode: true,
+    centerPadding: '10',
+    cssEase: 'linear',
+    slidesToShow:4,
+    slidesToScroll:1,
+    variableWidth: false,
+    adaptiveHeight:false,
+    infinite: true,
+    initialSlide: 1,
+    arrows: false,
+    buttons: false,
+    responsive:
             [ 
                 {
-                     breakpoint: 99999,
-                     settings: "unslick"
+                    breakpoint: 1300,
+                    settings: { slidesToShow:3 }
                 },
                 {
-                    breakpoint: 1003,
-                    settings: { slidesToShow:5 }
-                },
-                {
-                    breakpoint: 767,
-                    settings: { slidesToShow:4 }
-                },
-                {
-                    breakpoint: 599,
-                    settings: {  slidesToShow:3 }
-                },
-                {
-                    breakpoint: 479,
+                    breakpoint: 1023,
                     settings: { slidesToShow:2 }
                 },
                 {
-                    breakpoint: 300,
-                    settings: { slidesToShow:1  }
-                }
+                    breakpoint: 599,
+                    settings: { slidesToShow:1 }
+                } 
             ]
-        });
-    }
-});
+  });
+
 
  
 
