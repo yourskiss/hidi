ff6dbc86b41c64aae99535d9752c3a8e75007755
K. Singh <16361374+yourskiss@users.noreply.github.com>
2023-03-06 17:15:44 +0530

    ui

--- scripts/common.js
+++ scripts/common.js
@@ -23,7 +23,7 @@ $(".profileunlocks .profileunlockinner .profileunlockright ul li").click(functio
 });
  
 
-$('.talentslideRow1').slick({
+$('.talentslideRow1, .talentslideRow2').slick({
     speed: 6000,
     autoplay: true,
     autoplaySpeed: 0,
@@ -40,57 +40,25 @@ $('.talentslideRow1').slick({
     buttons: false,
     pauseOnHover: false,
     pauseOnFocus: false,
+    accessibility: false,
+    draggable: false,
     responsive:
             [ 
                 {
-                    breakpoint: 1300,
+                    breakpoint: 1255,
                     settings: { slidesToShow:3 }
                 },
                 {
-                    breakpoint: 1023,
+                    breakpoint: 767,
                     settings: { slidesToShow:2 }
                 },
                 {
-                    breakpoint: 599,
+                    breakpoint: 479,
                     settings: { slidesToShow:1 }
                 } 
             ]
   });
-  
-$('.talentslideRow2').slick({
-    speed: 6000,
-    autoplay: true,
-    autoplaySpeed: 0,
-    centerMode: false,
-    centerPadding: '0',
-    cssEase: 'linear',
-    slidesToShow:4,
-    slidesToScroll:1,
-    variableWidth: false,
-    adaptiveHeight:false,
-    infinite: true,
-    initialSlide:0,
-    arrows: false,
-    buttons: false,
-    pauseOnHover: false,
-    pauseOnFocus: false,
-    responsive:
-            [ 
-                {
-                    breakpoint: 1300,
-                    settings: { slidesToShow:3 }
-                },
-                {
-                    breakpoint: 1023,
-                    settings: { slidesToShow:2 }
-                },
-                {
-                    breakpoint: 599,
-                    settings: { slidesToShow:1 }
-                } 
-            ]
-  });
- 
+   
  
 
 
