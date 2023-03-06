e8a7ac1aaea240818437dd903970895ffc95d3db
K. Singh <16361374+yourskiss@users.noreply.github.com>
2023-03-06 16:21:16 +0530

    ui

--- scripts/common.js
+++ scripts/common.js
@@ -21,22 +21,21 @@ $(".profileunlocks .profileunlockinner .profileunlockright ul li").click(functio
     $(this).addClass("active");
     $(".profileunlocks .profileunlockinner .profileunlockleft div img[data-id='"+getDataID+"']").fadeIn(400);  
 });
+ 
 
-
-$('.talentslider').slick({
-    rows: 3,
+$('.talentslideRow1').slick({
     speed: 6000,
     autoplay: true,
     autoplaySpeed: 0,
-    centerMode: true,
-    centerPadding: '10',
+    centerMode: false,
+    centerPadding: '0',
     cssEase: 'linear',
     slidesToShow:4,
     slidesToScroll:1,
     variableWidth: false,
     adaptiveHeight:false,
     infinite: true,
-    initialSlide: 1,
+    initialSlide:0,
     arrows: false,
     buttons: false,
     pauseOnHover: false,
@@ -57,8 +56,41 @@ $('.talentslider').slick({
                 } 
             ]
   });
-
-
+  
+$('.talentslideRow2').slick({
+    speed: 6000,
+    autoplay: true,
+    autoplaySpeed: 0,
+    centerMode: false,
+    centerPadding: '0',
+    cssEase: 'linear',
+    slidesToShow:4,
+    slidesToScroll:1,
+    variableWidth: false,
+    adaptiveHeight:false,
+    infinite: true,
+    initialSlide:0,
+    arrows: false,
+    buttons: false,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    responsive:
+            [ 
+                {
+                    breakpoint: 1300,
+                    settings: { slidesToShow:3 }
+                },
+                {
+                    breakpoint: 1023,
+                    settings: { slidesToShow:2 }
+                },
+                {
+                    breakpoint: 599,
+                    settings: { slidesToShow:1 }
+                } 
+            ]
+  });
+ 
  
 
 
